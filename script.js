957afc5c0df6e939551cc1e5480a8e4cf3cf020a
RemingtonSteele <59892481+R-Steel3@users.noreply.github.com>
2020-12-01 17:44:55 -0500

    Updated JS code running better

--- script.js
+++ script.js
@@ -9,35 +9,27 @@ var passParts1 = (num2 + specials + let1 + num1 + let2);
 var passParts2 = (num1 + specials + let1 + num2 + let2);
 //generateBtnonclick: alert("Would you like to create a password");
 
-var passLength = prompt("How Many Characters?");
-var numConfim = confirm("Would you like Numbers?");
-var letUpConfirm = confirm("Would you like Uppercase Letters?");
-var specConfirm = confirm("Would you like Special Characters")
+
+
 
 
 //document.getElementById(generateBtn).addEventListener("click", function(){
   alert("Would you like to create a password?");
-//});
-if (generateBtn) {
-  (passLength);
+if (true) {
+  var passLength = prompt("How Many Characters?");
 }
 if (passLength >= 8) {
-  numConfim;
+ var numConfim = confirm("Would you like Numbers?"); 
 }
-else { alert("Password must be at LEAST 8 Characters")
+else { alert("Password must be at LEAST 8 Characters");
 }
-
-if (numConfim){
-  (letUpConfirm)
+if (numConfim === true) {
+  var letUpConfirm = confirm("Would you like Uppercase Letters?");
 }
-
-if (letUpConfirm){
-  (specConfirm)
+if (letUpConfirm === true){
+  var specConfirm = confirm("Would you like Special Characters")
 }
 
-function createPassword() {
-  return String.passParts1+String.passParts2(Math.random())
-}
 
 console.log()
 
